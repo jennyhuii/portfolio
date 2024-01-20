1b6ff8f0ed276c55d3ceca4141754c2ea7584218
Jenny <j.hui.0130@gmail.com>
2024-01-20 18:44:34 -0500

    layout and theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,57 @@ import * as React from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
+  Container,
   Grid,
-  theme,
+  Divider,
+  IconButton,
+  Flex,
+  Spacer,
+  Text,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { FaGithub, FaLinkedinIn } from "react-icons/fa"
+import '@fontsource/raleway/400.css'
+import '@fontsource/open-sans/700.css'
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Open Sans', sans-serif`,
+    body: `'Raleway', sans-serif`,
+  },
+})
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
+    <Box textAlign="center" fontSize="xl" p="2rem">
+      <Grid minH="90vh">
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <Container>Jenny Hui</Container>
+          <Container w='100%' h='100%'>Body</Container>
+          <Divider colorScheme="blue" />
+          <Container centerContent>
+            <VStack>
+            <Text>Get in touch</Text>
+            <Flex w="100%">
+              <IconButton
+                aria-label="github"
+                colorScheme="blue"
+                fontSize='2rem'
+                onClick={() => window.open("https://github.com/jennyhuii", "_blank")}
+                icon={<FaGithub />}
+              />
+              <Spacer />
+              <IconButton
+                aria-label="linkedin"
+                colorScheme="blue"
+                fontSize='2rem'
+                onClick={() => window.open("https://www.linkedin.com/in/jennyhui108/", "_blank")}
+                icon={<FaLinkedinIn />}
+              />
+            </Flex>
+            </VStack>
+          </Container>
         </VStack>
       </Grid>
     </Box>
